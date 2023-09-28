578f956ab68b7de30851d14fa6bb3a3182711825
Victor Hugo Targino <127980648+vhtargino@users.noreply.github.com>
2023-09-28 16:32:11 -0300

    Update lista1_exercicio6.py

--- Lista 1/lista1_exercicio6.py
+++ Lista 1/lista1_exercicio6.py
@@ -1,7 +1,6 @@
 # 6. Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em
 # graus Fahrenheit.
 
-graus_celsius = float(
-    input("Qual a temperatura atual em Celsius? Digite apenas números: "))
+graus_celsius = float(input("Qual a temperatura atual em Celsius? Digite apenas números: "))
 conversao_farenheit = graus_celsius * 1.8 + 32
 print(f"{graus_celsius}°C é o mesmo que {conversao_farenheit:.1f}°F")
