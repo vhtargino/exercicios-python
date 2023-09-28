df9b58fbcfbab3842c883b5777969134f7e1ac83
Victor Hugo Targino <127980648+vhtargino@users.noreply.github.com>
2023-09-28 16:31:58 -0300

    Update lista1_exercicio5.py

--- Lista 1/lista1_exercicio5.py
+++ Lista 1/lista1_exercicio5.py
@@ -2,7 +2,6 @@
 # a temperatura em graus Celsius.
 # C = 5 * ((F-32) / 9).
 
-graus_farenheit = float(
-    input("Qual a temperatura atual em Farenheit? Digite apenas números: "))
+graus_farenheit = float(input("Qual a temperatura atual em Farenheit? Digite apenas números: "))
 conversao_celsius = 5 * ((graus_farenheit - 32) / 9)
 print(f"{graus_farenheit:.1f}°F é o mesmo que {conversao_celsius:.1f}°C")
